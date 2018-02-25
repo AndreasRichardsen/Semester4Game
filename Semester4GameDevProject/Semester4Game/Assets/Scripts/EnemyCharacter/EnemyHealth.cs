e21ef37d358a2ea38bd96d0b9c258030165a4172
Andreas <andreas.w.richardsen@gmail.com>
2018-02-26 00:03:40 +0100

    HI

---
+++ Semester4GameDevProject/Semester4Game/Assets/Scripts/EnemyCharacter/EnemyHealth.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour {
+
+    public int startingHealth = 100;
+    public int currentHealth;
+    public int scoreValue = 10;
+
+    CapsuleCollider capsuleCollider;
+    bool isDead;
+
+    void Awake()
+    {
+        capsuleCollider = GetComponent<CapsuleCollider>();
+
+        currentHealth = startingHealth;
+    }
+
+    public void TakeDamage(int amount, Vector3 hitPoint)
+    {
+        if (isDead)
+        {
+            return;
+        }
+
+        currentHealth -= amount;
+
+        if(currentHealth <= 0)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        isDead = true;
+
+        capsuleCollider.isTrigger = true;
+    }
+}
